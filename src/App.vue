33f42d62ba4fe3fb0ea20181f96562b59a308580
stjepanknezevic <kstjepan4@gmail.com>
2024-11-10 15:34:07 +0100

    feat: changed vue to show built unity unsucessfully

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import UnityWebgl from 'unity-webgl'
+import VueUnity from 'unity-webgl/vue'
+
+const unityContext = new UnityWebgl({
+  loaderUrl: './Build/Build.loader.js',
+  dataUrl: './Build/Build.data.br',
+  frameworkUrl: './Build/Build.framework.js.br',
+  codeUrl: './Build/Build.wasm.br',
+})
+
+unityContext.on('device', () => alert('click device...'))
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <VueUnity :unity="unityContext" width="1920" height="1080" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {DiscordSDK} from "@discord/embedded-app-sdk"
 
 const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
@@ -9,10 +9,8 @@ async function setupDiscordSdk(){
     await discordSdk.ready();
 }
 
-setupDiscordSdk().then(()=>{
-  console.log("Discord SDK is authenticated");
+setupDiscordSdk().then(() => {
+    console.log("Discord SDK is authenticated");
 });
-
-
 
 createApp(App).mount('#app')
